7b2cc2ddd9e1800ad93313507cc7fcfa2b326809
Luis Pedro Coelho <lpc@cmu.edu>
2010-01-31 14:37:37 -0500

    Make thie more flexible

--- templates/latex/build.sh
+++ templates/latex/build.sh
@@ -1,4 +1,12 @@
-mkdir -p .tex_files
-cd .tex_files
-TEXINPUTS=..:../cmu-beamer/:.:../images/:../figures/: pdflatex presentation
-cp presentation.pdf ..
+#!/usr/bin/zsh
+function build() {
+    input=$1
+    mkdir -p .$input.tex_files
+    cd .$input.tex_files
+    TEXINPUTS=..:../cmu-beamer/:.:../images/:../figures/: pdflatex $input
+    BSTINPUTS=:..: BIBINPUTS=:..:.: bibtex $texfile
+    cp $input.pdf ..
+    cd ..
+}
+build presentation
+
