2149393674e7b5fedadb4f7b1736a92a11fc0f9d
Luis Pedro Coelho <lpc@cmu.edu>
2011-04-12 19:49:45 -0400

    ENH Fix paths for build.sh
    Otherwise, latex+bibtex does not work

--- templates/latex/build.sh
+++ templates/latex/build.sh
@@ -3,7 +3,7 @@ function build() {
     input=$1
     mkdir -p .$input.tex_files
     cd .$input.tex_files
-    TEXINPUTS=..:../cmu-beamer/:.:../images/:../figures/: pdflatex $input
+    TEXINPUTS=.:..:../cmu-beamer/:.:../images/:../figures/: pdflatex $input
     BSTINPUTS=:..: BIBINPUTS=:..:.: bibtex $input
     cp $input.pdf ..
     cd ..
