4a01c2dcf3548b838b4aedbdabb717034fe349da
Luis Pedro Coelho <luis@luispedro.org>
2021-10-17 15:42:15 +0200

    ENH Update to newer ubuntu packages

--- basics/install_skype.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-sudo apt-get update && sudo apt-get install skype
-
--- basics/python_virtualenv.sh
+++
@@ -1,4 +0,0 @@
-cd $HOME
-virtualenv .virtualenv
-source .virtualenv/bin/activate
-pip install nose
